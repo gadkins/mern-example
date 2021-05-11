52385982e63381ca402c41d890ca0495590a65b0
Grayson Adkins <grayson@idyl.tech>
2021-05-11 14:43:45 -0500

    change blue button to green

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -65,7 +65,7 @@ class Form extends Component {
           label="Position"
         />
 
-        <Button variant="contained" color="primary" onClick={this.submit}> Submit </Button>
+        <Button variant="contained" color="green" onClick={this.submit}> Submit </Button>
 
       </form>
     );
